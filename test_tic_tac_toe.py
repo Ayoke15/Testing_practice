641c193bf249706e79ca1965616936d5e813db6a
Arina Pashkova <arina.pashkova@list.ru>
2023-10-08 23:30:41 +0300

    Add game interface

--- test_tic_tac_toe.py
+++ test_tic_tac_toe.py
@@ -23,5 +23,17 @@ class TestTicTacToe(unittest.TestCase):
         game.board = [["X", "O", "X"], ["X", "X", "O"], ["O", "X", "O"]]
         self.assertEqual(game.check_winner(), "Draw")
 
+    def test_is_cell_empty(self):
+        game = TicTacToe()
+        self.assertTrue(game.is_cell_empty(0, 0))
+        game.add_X(0, 0)
+        self.assertFalse(game.is_cell_empty(0, 0))
+
+    def test_switch_turn(self):
+        game = TicTacToe()
+        self.assertEqual(game.turn, "X")
+        game.switch_turn()
+        self.assertEqual(game.turn, "O")
+
 if __name__ == "__main__":
     unittest.main()
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -2,6 +2,7 @@ class TicTacToe:
 
     def __init__(self):
         self.board = [[" ", " ", " "] for _ in range(3)]
+        self.turn = "X"
 
     def add_X(self, row, col):
         self.board[row][col] = "X"
@@ -23,3 +24,38 @@ class TicTacToe:
         if all(cell != " " for row in self.board for cell in row):
             return "Draw"
         return None
+
+    def is_cell_empty(self, row, col):
+        return self.board[row][col] == " "
+
+    def switch_turn(self):
+        self.turn = "O" if self.turn == "X" else "X"
+
+    def display_board(self):
+        print("  1 2 3")
+        for i, row in enumerate(self.board):
+            print(f"{i + 1} {' '.join(row)}")
+
+if __name__ == "__main__":
+    game = TicTacToe()
+
+    while True:
+        game.display_board()
+        row = int(input(f"Player {game.turn}, enter the row (1-3): ")) - 1
+        col = int(input(f"Player {game.turn}, enter the column (1-3): ")) - 1
+
+        if game.is_cell_empty(row, col):
+            if game.turn == "X":
+                game.add_X(row, col)
+            else:
+                game.add_O(row, col)
+        else:
+            print("Cell is already occupied. Try again.")
+            continue
+
+        winner = game.check_winner()
+        if winner:
+            print(f"The winner is {winner}!")
+            break
+
+        game.switch_turn()
